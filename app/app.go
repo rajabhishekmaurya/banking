73b845f49d2c2e1cf5355975cd91df12d20dddca
abhishek.maurya <abhishek.maurya@npci.org.in>
2024-04-07 11:20:04 +0530

    Post method added

--- app/app.go
+++ app/app.go
@@ -1,18 +1,33 @@
 package app
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 
 func Start(){
-	mux := http.NewServeMux()
+	// mux := http.NewServeMux()
+
+	router := mux.NewRouter()
 
 	//define routes
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/customers", getAllCustomers)
-
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomerById).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
 	//starting the server
-	log.Fatal(http.ListenAndServe("localhost:8000", mux))
+	log.Fatal(http.ListenAndServe("localhost:8000", router))
+}
+
+func getCustomerById(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	fmt.Fprint(w, vars["customer_id"])
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "Post request recieved")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rajabhishekmaurya/banking
 
 go 1.21.5
+
+require github.com/gorilla/mux v1.8.1
